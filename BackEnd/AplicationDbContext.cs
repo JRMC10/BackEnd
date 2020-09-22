cc168f9287fe6d86eab6b3441e872e004494bd9f
rafael cruz <rafita_1320@hotmail.com>
2020-09-22 13:28:49 -0500

    test

---
+++ BackEnd/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class AplicationDbContext: DbContext
+    {
+        //constructor
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
