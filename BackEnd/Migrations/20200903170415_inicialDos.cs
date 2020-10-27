3ec14f95caa4c818d5fb7f706dc9a2a7086bdc8f
rafael cruz <rafita_1320@hotmail.com>
2020-10-27 14:56:30 -0600

    VERSION1.2

---
+++ BackEnd/Migrations/20200903170415_inicialDos.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class inicialDos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaExp",
+                table: "TarjetaCredito");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FechaExpiracion",
+                table: "TarjetaCredito",
+                type: "varchar(5)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaExpiracion",
+                table: "TarjetaCredito");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FechaExp",
+                table: "TarjetaCredito",
+                type: "varchar(10)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
